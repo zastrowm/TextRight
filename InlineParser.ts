137e06698725c042cea9d3402e3472cd8efa1ac2
Mackenzie Zastrow <3211021+zastrowm@users.noreply.github.com>
2014-07-29 18:39:47 -0500

    Initial Commmit

---
+++ InlineParser.ts
@@ -0,0 +1,15 @@
+﻿module TextRight {
+  /** Takes a position of content and a line number to be passed between functions */
+  export class LineContent {
+    constructor(public text: TextAndPosition, public line: number) {
+    }
+  }
+
+  /** Takes an array of LineContents so it can be treated as a single block of text. */
+  export class ContentStream {
+    constructor(content: LineContent[]) {
+    }
+  }
+
+  //export function parseInlineContent()
+}
