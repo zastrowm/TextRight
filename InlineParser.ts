4cf2ecc67c0516e078406a5817292be3f4dc2f88
Mackenzie Zastrow <3211021+zastrowm@users.noreply.github.com>
2014-08-02 17:50:02 -0500

    Finalize parsing of blocks

--- InlineParser.ts
+++ InlineParser.ts
@@ -1,16 +1,4 @@
 ﻿module TextRight {
-  /** Takes a position of content and a line number to be passed between functions */
-  export class LineContent {
-    constructor(public text: LineFragment, public line: number) {
-    }
-  }
-
-  /** Takes an array of LineContents so it can be treated as a single block of text. */
-  export class ContentStream {
-    constructor(content: LineContent[]) {
-    }
-  }
-
   enum NodeType {
     Heading,
     Style,
