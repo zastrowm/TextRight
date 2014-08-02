09d767692d46c91d998715d9bcd51f4a043c4700
Mackenzie Zastrow <3211021+zastrowm@users.noreply.github.com>
2014-08-02 18:23:39 -0500

    Make lines more abstract so that LineFragments can be used in the LineParser

---
+++ NodeParser.ts
@@ -0,0 +1,15 @@
+﻿module TextRight {
+  function parse(blocks: Block[]) {
+  }
+
+  export class NodeParser {
+    constructor(
+      private options: Options = new Options(),
+      private blockParser: BlockParser = new BlockParser(options),
+      private inlineParser: InlineParser = new InlineParser(options)) {
+    }
+
+    public parse(blocks: Block[]) {
+    }
+  }
+} 
